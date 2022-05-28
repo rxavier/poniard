1690256b82fa2f0b49ff4b247f1eb33f0d1a1bdc
Rafael Xavier <rxaviermontero@gmail.com>
2022-05-28 01:39:57 -0300

    Add pandas_profiling

--- poniard/plugins/__init__.py
+++ poniard/plugins/__init__.py
@@ -1,3 +1,4 @@
 from poniard.plugins.wandb import WandBPlugin
+from poniard.plugins.pandas_profiling import PandasProfilingPlugin
 
-__all__ = ["WandBPlugin"]
+__all__ = ["WandBPlugin", "PandasProfilingPlugin"]
---
+++ poniard/plugins/pandas_profiling.py
@@ -0,0 +1,71 @@
+import importlib
+from typing import Union
+from pathlib import Path
+
+import pandas as pd
+import numpy as np
+from pandas_profiling import ProfileReport
+
+from poniard.plugins.core import BasePlugin
+
+
+class PandasProfilingPlugin(BasePlugin):
+    """Pandas Profiling plugin. Kwargs from the constructor are passed to `ProfileReport()`.
+
+    Parameters
+    ----------
+    title :
+        Report title.
+    explorative :
+        Enable explorative mode. Default False.
+    minimal :
+        Enable minimal mode. Default True.
+    html_path :
+        Path where the HTML report will be saved. Default is the title of the report.
+    """
+
+    def __init__(
+        self,
+        title: str,
+        explorative: bool = False,
+        minimal: bool = True,
+        html_path: Union[str, Path] = None,
+        **kwargs
+    ):
+        self.title = title
+        self.explorative = explorative
+        self.minimal = minimal
+        self.html_path = html_path or Path(self.title + ".html")
+        self.kwargs = kwargs or {}
+
+    def on_setup_end(self) -> None:
+        """Create Pandas Profiling HTML report."""
+        X, y = self._poniard.X, self._poniard.y
+        try:
+            dataset = pd.concat([X, y], axis=1)
+        except TypeError:
+            if y.ndim == 1:
+                dataset = np.concatenate([X, np.expand_dims(y, 1)], axis=1)
+            else:
+                dataset = np.concatenate([X, y], axis=1)
+            dataset = pd.DataFrame(dataset)
+        self.report = ProfileReport(
+            df=dataset,
+            minimal=self.minimal,
+            explorative=self.explorative,
+            **self.kwargs
+        )
+        self.report.to_file(self.html_path)
+        self._log_to_wandb_if_available()
+        self.report.to_notebook_iframe()
+        return
+
+    def _log_to_wandb_if_available(self):
+        if importlib.util.find_spec("wandb"):
+            import wandb
+            from poniard.plugins import WandBPlugin
+
+            if any(isinstance(x, WandBPlugin) for x in self._poniard.plugins):
+                with open(self.html_path) as report:
+                    wandb.log({"profile_report": wandb.Html(report)})
+        return
