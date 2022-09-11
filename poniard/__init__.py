3c9ed57153099b5153e078af3d6c2823cc8dcf36
Rafael Xavier <rxaviermontero@gmail.com>
2022-09-11 20:28:47 -0300

    nbdev first try

--- poniard/__init__.py
+++ poniard/__init__.py
@@ -1,3 +1,4 @@
+__version__ = "0.2.3"
 from poniard._version import __version__
 from poniard.estimators.classification import PoniardClassifier
 from poniard.estimators.regression import PoniardRegressor
